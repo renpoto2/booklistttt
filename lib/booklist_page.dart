bf0c7137234c76b92260e20c698619d297801531
renpoto2 <renpoto2@icloud.com>
2021-11-05 01:27:23 +0900

    要らない所削除

--- lib/booklist_page.dart
+++ lib/booklist_page.dart
@@ -5,48 +5,6 @@ import 'package:flutter/material.dart';
 import 'addition/movie.dart';
 
 
-class bookList extends StatelessWidget {
-  final Stream<QuerySnapshot> _usersStream =
-      FirebaseFirestore.instance.collection('books').snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("boookliiist!"),
-
-      ),
-      body: Center(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: _usersStream,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return Text("Loading");
-            }
-
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-                return ListTile(
-                  title: Text(data['title']),
-                  subtitle: Text(data['author']),
-                );
-              }).toList(),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
 class FirstPage extends StatelessWidget{
   FirstPage(this.Num);
   int Num;
